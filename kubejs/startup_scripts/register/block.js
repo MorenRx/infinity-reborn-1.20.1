b8b49b0d29d7a3d3f5cde1bebf7e284abdab19fc
MorenRx <morenrx1@gmail.com>
2025-03-11 20:39:22 +0800

    更新
    添加并修改部分配置
    去除几个Mod
    实现随机标题
    禁用原本的地狱门
    添加一个新的地狱门

--- kubejs/startup_scripts/register/block.js
+++ kubejs/startup_scripts/register/block.js
@@ -1,8 +1,19 @@
 StartupEvents.registry('block', event => {
+	// 精金锻炉砖
 	event.create('adamantite_brick')
 		.material('metal')
 		.hardness(3.0)
 		.tagBlock('minecraft:mineable/pickaxe')
 		.tagBlock('minecraft:needs_iron_tool')
 		.requiresTool(true)
+	
+	// 特殊处理的黑曜石
+	event.create('special_obsidian')
+		.material('stone')
+		.hardness(75.0)
+		.resistance(1200.0)
+		.tagBlock('minecraft:mineable/pickaxe')
+		.tagBlock('minecraft:needs_diamond_tool')
+		.requiresTool(true)
+	
 })
